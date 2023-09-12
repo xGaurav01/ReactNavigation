5e3fbfbcccd55b878fd45fa7928b242f82915217
xGaurav01 <gauravsharma76500@gmail.com>
2023-09-12 17:16:51 +0530

    Second push

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+const styles=StyleSheet.create({
+  
+})
